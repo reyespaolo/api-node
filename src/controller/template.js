69128ed029d0d039366ebcc1f51fef8df50412a7
Paolo Reyes <reyes.paolo@360logix.com>
2018-03-12 13:54:55 +0800

    Add, Update, Read and Delete

--- src/controller/template.js
+++ src/controller/template.js
@@ -10,15 +10,69 @@ export default({ config, db }) => {
 
   // '/v1/restaurant/add'
   api.post('/add', (req, res) => {
-    let newTemp = new Template();
-    newTemp.name = req.body.name;
+    var body = _.pick(req.body, ['name'])
+    let newTemp = new Template(body);
 
     newTemp.save(function(err) {
       if (err) {
         res.send(err);
       }
-      res.json({ message: 'Restaurant saved successfully' });
+      res.json({ message: 'Template saved successfully' });
     });
   });
+
+  // Read All
+  api.get('/', (req, res) =>{
+    Template.find({}, (err, temp) => {
+      if(err){
+        res.send(err);
+      }
+      res.json(temp);
+    });
+  });
+
+  // Read per ID Read 1
+  api.get('/:id',(req, res) => {
+    Template.findById(req.params.id, (err, temp) => {
+      if(err){
+        res.send(err);
+      }
+      res.json(temp);
+    });
+  });
+
+  api.put('/:id', (req, res) => {
+    Template.findById(req.params.id, (err, temp) => {
+      if(err){
+        res.send(err);
+      }
+      var body = _.pick(req.body, ['name'])
+      temp.name = body.name
+      temp.save(err => {
+        if(err){
+          res.send(err)
+        }
+        res.json({
+          message: "Restaurant Info Updated",
+          data: temp
+        });
+      });
+    });
+  });
+
+  api.delete('/:id', (req, res) => {
+    Template.remove({
+      _id:req.params.id
+    }, (err, template) =>{
+      if(err){
+        res.send(err);
+      }
+      res.json({
+        message: "Template Successfully Removed",
+        data: template
+      })
+    })
+  })
+
   return api;
 }
