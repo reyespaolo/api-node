f0243ef30995deb34049d9966d8911867f753f0a
Paolo Reyes <reyes.paolo@360logix.com>
2018-08-04 10:36:46 +0800

    MongoDB useNewURLParser

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 require('./config');
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
 module.exports = { mongoose }
